637ea559717595772c09eb36a857ef694b628269
jason-qs <sanchezjason2001@hotmail.com>
2023-01-27 21:54:29 -0700

    add build and deploy to workflow

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,7 @@ function Home() {
 
     <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
       <div className="col-md-5 p-lg-5 mx-auto my-5">
-        <h1 className="display-4 font-weight-normal">Business Contacts</h1>
+        <h1 className="display-4 font-weight-normal">Business Contact</h1>
         <p className="lead font-weight-normal">"It's better to hang out with people better than you. Pick out associates whose behavior is better than yours and you'll drift in that direction."</p>
         <p className="lead font-weight-normal"><em>- Warren Buffett</em></p>
         <Link className="btn btn-outline-secondary" to="/contacts">View Contacts</Link>
